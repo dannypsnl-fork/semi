226da260f48589c11a2853c75bf527cfbcdd7e50
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-03-09 13:18:38 +0800

    reorg

--- main.rkt
+++ main.rkt
@@ -9,32 +9,24 @@
 
   (define U (syntax-property #''U
                              'type
-                             'U))
+                             'U)))
 
-  (define Nat (syntax-property #''Nat
-                               'type
-                               U))
-
-  (define zero (syntax-property #''zero
-                                'type
-                                Nat))
-
-  (define (suc n) (syntax-property #`'(suc #,n)
-                                   'type
-                                   Nat)))
-
+(define-for-syntax Nat (syntax-property #''Nat
+                                        'type U))
+(define-for-syntax zero (syntax-property #''zero
+                                         'type Nat))
 (define-syntax (zero stx)
   (syntax-parse stx
     [x zero]))
-
-zero
-
+(define-for-syntax (suc n) (syntax-property #`'(suc #,n)
+                                            'type Nat))
 (define-syntax (suc stx)
   (syntax-parse stx
     [(_ n)
-     (define new-n (local-expand #'n 'expression null))
-     (unify (eval Nat) (typeof new-n)
-            stx #'n)
-     (suc (eval new-n))]))
+     (let ([new-n (local-expand #'n 'expression null)])
+       (unify (eval Nat) (typeof new-n)
+              stx #'n)
+       (suc (eval new-n)))]))
 
+zero
 (suc zero)
